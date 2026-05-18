a312dc9f9fa9cef78c2f57c0eccef1786b79fa5f
Gaveen Perera <gaveen@example.com>
2026-05-18 14:06:48 +0530

    removed unwated code  and made some changes

--- api/cron/index.js
+++
@@ -1,7 +0,0 @@
-export default function handler(req, res) {
-  if (req.headers['authorization'] !== `Bearer ${process.env.CRON_SECRET}`) {
-    return res.status(401).json({ error: 'Unauthorized' });
-  }
-  // your cron logic
-  return res.status(200).json({ ok: true });
-}
